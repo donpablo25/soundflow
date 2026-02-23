c81efb4ef45fbad0d02c0f7476b5354dc065501f
Adam <adambouayad1@outlook.com>
2026-02-22 22:26:09 -0500

    Refactorisation des services Auth et Firestore

--- src/back/auth.js
+++ src/back/auth.js
@@ -1,5 +1,5 @@
-import { auth, db } from "./firebase";
-
+import { auth, db, storage } from "./firebase";
+import { CreateUsers } from "./firestore";
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
@@ -9,13 +9,16 @@ import {
   signOut,
   GoogleAuthProvider
 } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const googleProvider = new GoogleAuthProvider();
 
 export function loginWithEmail(email, password) {
   return signInWithEmailAndPassword(auth, email.trim(), password);
 }
 
-export async function registerWithEmail(email, password, name, role="auditeur") {
+export async function registerWithEmail(email, password, name, role = "auditeur") {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email.trim(), password);
     const user = userCredential.user;
@@ -24,13 +27,12 @@ export async function registerWithEmail(email, password, name, role="auditeur") 
       displayName: name
     });
 
-    await setDoc(doc(db, "users", user.uid), {
+    await CreateUsers({
       uid: user.uid,
-      name: name,
-      email: email.trim(),
-      role: role,
-      avatarUrl: "",
-      createdAt: Date.now()
+      displayName: name,
+      email: user.email,
+      photoURL: "",
+      role: role
     });
 
     return userCredential;
@@ -40,10 +42,43 @@ export async function registerWithEmail(email, password, name, role="auditeur") 
   }
 }
 
-const googleProvider = new GoogleAuthProvider()
+export const updateUserProfile = async (user, { username, photoFile, currentPhotoURL, bio }) => {
+  try {
+    let finalPhotoURL = currentPhotoURL;
 
-export function loginWithGoogle() {
-  return signInWithPopup(auth, googleProvider);
+    if (photoFile) {
+      const storageRef = ref(storage, `avatars/${user.uid}`);
+      await uploadBytes(storageRef, photoFile);
+      finalPhotoURL = await getDownloadURL(storageRef);
+    }
+
+    await updateProfile(user, {
+      displayName: username,
+      photoURL: finalPhotoURL
+    });
+
+    const userRef = doc(db, "users", user.uid);
+    await setDoc(userRef, { 
+      bio: bio,
+      displayName: username, 
+      photoURL: finalPhotoURL
+    }, { merge: true });
+
+  } catch (error) {
+    console.error("Erreur dans updateUserProfile:", error);
+    throw error;
+  }
+};
+
+export async function loginWithGoogle() {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    await CreateUsers(result.user);
+    return result;
+  } catch (error) {
+    console.error("Erreur Google Login:", error);
+    throw error;
+  }
 }
 
 export function logout() {
